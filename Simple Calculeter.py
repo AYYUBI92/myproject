8c5725d0e6b96a8e6b71df9c36ab64ccb3e006d1
Danish Ayyubi <76676459+AYYUBI92@users.noreply.github.com>
2020-12-27 22:06:11 -0800

    first commid

--- Simple Calculeter.py
+++ Simple Calculeter.py
@@ -1,6 +1,9 @@
 num1 = int(input("Enter Your First Numbers: "))
+#Create First User input Var
 operator = input("Enter Operator")
+#Create Operator Variable
 num2 = int(input("Enter Your Second Numbers: "))
+#Create Second User Input Variable
 if operator == "+":
     print(num1 + num2 )
 elif operator == "-":
